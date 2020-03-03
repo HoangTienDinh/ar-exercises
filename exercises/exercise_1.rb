36769ba9eb5b86bb4a4df43c39c36b31ab509ae0
HoHoHoang <hoangdinh90@gmail.com>
2020-03-03 00:07:47 +0000

    finished exercise 1 & 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,12 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Your code goes here ...
+
+storeOne = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+storeTwo = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+storeThree = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "Here are the number of stores: #{Store.count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+# puts @store1.annual_revenue
+
+@store1.update(annual_revenue: 360000)
+# puts @store1.annual_revenue
+
 # Your code goes here ...
