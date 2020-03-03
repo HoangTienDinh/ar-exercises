f17c10c8a0e2de91b5865cef102bad463de48f57
HoHoHoang <hoangdinh90@gmail.com>
2020-03-03 00:30:21 +0000

    @mens_stores calls working for exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+storeFour = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+storeFive = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+storeFour = Store.create(name: "Yale Town", annual_revenue: 430000, mens_apparel: false, womens_apparel: true)
+
+# puts Store.find_by(mens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each { |s| puts "The store #{s.name} has an annual revenue of #{s.annual_revenue}"}
